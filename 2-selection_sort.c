5a378a121f86d6a5e9fc5ec5f58775f707be2a93
SpStigma <githubstigma@gmail.com>
2023-09-18 23:29:28 +0200

    Fix: New condition to check array NULL and size < 2.

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -13,6 +13,11 @@ void selection_sort(int *array, size_t size)
 	size_t min_index = 0;
 	int temp;
 
+	if (array == 0 || size < 2)
+	{
+		return;
+	}
+
 	for (i = 0; i < size - 1; i++)
 	{
 		min_index = i;
