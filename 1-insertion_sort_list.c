56379b502b35b9b1c0b2ecdd9d5c10ffd618b504
SpStigma <githubstigma@gmail.com>
2023-09-18 22:28:20 +0200

    Fix: emplacement of declaration

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,10 +8,11 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *current, *temp;
+    
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	listint_t *current, *temp;
 
 	current = (*list)->next;
 
