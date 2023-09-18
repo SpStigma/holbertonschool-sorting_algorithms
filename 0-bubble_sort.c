b5fdda78770c715bf90008cfe2d56110d0d427bd
SpStigma <githubstigma@gmail.com>
2023-09-18 13:04:13 +0200

    Fix: swap in the loop to swap number.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,10 +23,9 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
-
-				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
+				tmp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = tmp;
 				print_array(array, size);
 			}
 		}
