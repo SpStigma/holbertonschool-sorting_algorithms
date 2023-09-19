cc0fbe075cb7b075f7996b35c2c1ecd2cea8a307
SpStigma <githubstigma@gmail.com>
2023-09-20 00:36:53 +0200

    Completion of quick_sort using Lomuto partition.

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -13,46 +13,55 @@ void swap(int *i, int *j)
 /**
  * partition - partitions an array using Lomuto scheme.
  * @array: Pointer to the array.
- * @l: Starting index of the partition.
- * @r: Ending index of the partition.
+ * @low: Starting index of the partition.
+ * @high: Ending index of the partition.
  * @size: size of array.
  * Return: index of the pivot element.
  */
-int partition(int *array, int l, int r, size_t size)
+int partition(int *array, int low, int high, size_t size)
 {
-	int pivot = array[r];
-	int i = l;
+	int pivot = array[high];
+	int i = low - 1;
+	int j = 0;
 
-	for (int j = l; j < r; ++j)
+	for (j = low; j < high; j++)
 	{
-		if (array[j] < pivot)
+		if (array[j] <= pivot)
 		{
-			swap(&array[i], &array[j]);
-			print_array(array, size);
-			++i;
+			i++;
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
 		}
 	}
-	swap(&array[i], &array[r]);
-	print_array(array, size);
-	return (i);
+	if (array[i + 1] != array[high])
+	{
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
+	return (i + 1);
 }
+
 /**
  * quicksort_recursion - recursive function to perform quick sort.
  * @array: Pointer to the array.
- * @l: Starting index of the partition.
- * @r: Ending index of the partition.
+ * @low: Starting index of the partition.
+ * @high: Ending index of the partition.
  * @size: size of array.
  */
-
-void quicksort_recursion(int *array, int l, int r, size_t size)
+void quicksort_recursion(int *array, int low, int high, size_t size)
 {
-	if (l >= r)
-		return;
+	int Lomuto;
 
-	int i = partition(array, l, r, size);
+	if (low < high)
+	{
+		Lomuto = partition(array, low, high, size);
 
-	quicksort_recursion(array, l, i - 1, size);
-	quicksort_recursion(array, i + 1, r, size);
+		quicksort_recursion(array, low, Lomuto - 1, size);
+		quicksort_recursion(array, Lomuto + 1, high, size);
+	}
 }
 
 /**
@@ -61,13 +70,10 @@ void quicksort_recursion(int *array, int l, int r, size_t size)
  * @array: Pointer to the array.
  * @size:  Size of the array.
  */
-
 void quick_sort(int *array, size_t size)
 {
-	if (size <= 1)
-	return;
+	if (size < 2)
+		return;
 
 	quicksort_recursion(array, 0, size - 1, size);
 }
-
-
